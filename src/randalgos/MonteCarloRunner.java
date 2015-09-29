adab384db7b4ca90c8a984decf2a91f192065e4e
Trent Holliday <tholliday@morristechnology.com>
2015-09-29 08:28:40 -0400

    Added some much needed documentation the MonteCarlo simulation.

--- src/randalgos/MonteCarloRunner.java
+++ src/randalgos/MonteCarloRunner.java
@@ -35,6 +35,11 @@ import java.util.*;
  */
 public class MonteCarloRunner {
 
+    /**
+     * Computes the results of running the Monte Carlo algorithm to approximate the area under the curve for the
+     * equations.
+     * @return an ArrayList containing the results of the algorithm.
+     */
     private static ArrayList<MonteCarloResult> exec(){
         // Object that will be used to compute the area under the curves
         MonteCarlo monte_carlo = new MonteCarlo();
@@ -95,6 +100,7 @@ public class MonteCarloRunner {
 
         for (Equation equation : equation_list){
             for(int num_times : number_of_elements) {
+                // Approximate the area
                 Double equation_answer = monte_carlo.calculate_area(equation, num_times);
                 equation_results.add(new MonteCarloResult(equation, num_times, equation_answer));
             }
@@ -103,23 +109,38 @@ public class MonteCarloRunner {
         return equation_results;
     }
 
+    /**
+     * Method that is called in order to begin the monte carlo simulation and to display the results in a window.
+     */
     public static void show(){
         TableView table = new TableView();
+        String css_file = MonteCarloRunner.class.getResource("styles/javafx-tables.css").toExternalForm();
+        table.getStylesheets().add(css_file);
+
         ObservableList<MonteCarloResult> table_data = FXCollections.observableArrayList();
 
+        // Run the monte carlo algorithm
         ArrayList<MonteCarloResult> monteCarloResults = exec();
+
+        // Add the results to the data that will be put into our table
         table_data.addAll(monteCarloResults);
+        // Add the data to the table
         table.getItems().addAll(table_data);
 
         Stage monteCarloStage = new Stage();
         Scene scene = new Scene(new Group());
 
+        // Setup the tabbed interface
         TabPane tabPane = new TabPane();
         tabPane.setPadding(new Insets(29, 0, 0, 0));
         tabPane.setSide(Side.RIGHT);
         tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
+
+        // We store which tab was open last so that when we 'Rerun' the simulation the window is opened up to the
+        // tab that we were on before.
         tabPane.getSelectionModel().select(last_active_tab);
 
+        // Create a menu which will be used to rerun our algorithm
         MenuBar menuBar = new MenuBar();
         menuBar.autosize();
         Menu file = new Menu("File");
@@ -128,36 +149,20 @@ public class MonteCarloRunner {
         rerun.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
+                // Before we close the program we want to save which tab is open so that when we restore the window
+                // it gets opened up to the same tab.
                 last_active_tab = tabPane.getSelectionModel().getSelectedIndex();
                 monteCarloStage.close();
+                // Recompute the results of the algorithm
                 MonteCarloRunner.show();
             }
         });
         file.getItems().addAll(rerun);
 
         menuBar.getMenus().addAll(file);
+        // Makes the menu bar have the same width as the window
         menuBar.prefWidthProperty().bind(monteCarloStage.widthProperty());
 
-        int width = 1200;
-        int height = 850;
-
-        monteCarloStage.setTitle("Monte Carlo Algorithm - Area Under Curve");
-
-        monteCarloStage.setWidth(width);
-        monteCarloStage.setHeight(height);
-
-        table.setPrefWidth(width);
-        table.setPrefHeight(height);
-
-        monteCarloStage.widthProperty().addListener(
-                (observable, oldValue, newValue) ->
-                        tabPane.setPrefWidth(newValue.doubleValue())
-        );
-
-        monteCarloStage.heightProperty().addListener(
-                (observable, oldValue, newValue) ->
-                        tabPane.setPrefHeight(newValue.doubleValue() - 27)
-        );
 
         BorderPane graphPanel = new BorderPane();
         FlowPane graphKey = new FlowPane();
@@ -172,7 +177,7 @@ public class MonteCarloRunner {
         Tab tableTab = new Tab("Table data");
 
         // Create the table columns
-        TableColumn[] tableColumns = populate_table(table);
+        TableColumn[] tableColumns = create_table_columns();
 
         table.getColumns().addAll(tableColumns);
         tableTab.setContent(table);
@@ -182,6 +187,21 @@ public class MonteCarloRunner {
 
         tabPane.getTabs().addAll(tableTab, graphTab);
 
+        monteCarloStage.setTitle("Monte Carlo Algorithm - Area Under Curve");
+
+        monteCarloStage.setWidth(width);
+        monteCarloStage.setHeight(height);
+
+        table.setPrefWidth(width);
+        table.setPrefHeight(height);
+
+        tabPane.prefWidthProperty().bind(monteCarloStage.widthProperty());
+
+        monteCarloStage.heightProperty().addListener(
+                (observable, oldValue, newValue) ->
+                        tabPane.setPrefHeight(newValue.doubleValue() - 27)
+        );
+
         ((Group) scene.getRoot()).getChildren().addAll(tabPane, menuBar);
         monteCarloStage.setScene(scene);
         Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
@@ -190,7 +210,13 @@ public class MonteCarloRunner {
         monteCarloStage.show();
     }
 
-    private static TableColumn[] populate_table(TableView table){
+    /**
+     * Creates the columns that will be used in the table that displays the results of the simulation. The values
+     * used in the columns are taken from the Properties on the MonteCarlo result.
+     *
+     * @return an array of the TableColumns created.
+     */
+    private static TableColumn[] create_table_columns(){
         TableColumn<MonteCarloResult, String> equation = new TableColumn<>("Equation");
         equation.setCellValueFactory(
                 (param) ->
@@ -229,17 +255,21 @@ public class MonteCarloRunner {
         );
         error_percent.setPrefWidth(120);
 
-        String css_file = MonteCarloRunner.class.getResource("styles/javafx-tables.css").toExternalForm();
-        table.getStylesheets().add(css_file);
-
-        TableColumn[] table_columns = {equation, number_of_times, approx_result, actual_result, error_percent};
-        return table_columns;
+        return new TableColumn[]{equation, number_of_times, approx_result, actual_result, error_percent};
     }
 
     private static String create_formatted_string(Double number){
         return String.format("%.4f", number);
     }
 
+    /**
+     * Creates a line graph that represents the error percentage for each of the equations with respect to the number
+     * of guesses taken.
+     *
+     * @param graphKey The panel that will house the equation images for reference.
+     * @param table_data The results of the simulation
+     * @return a line graph with each equation's results plotted.
+     */
     private static LineChart populate_graph(FlowPane graphKey, ObservableList<MonteCarloResult> table_data){
         final NumberAxis xAxis = new NumberAxis();
         xAxis.setAutoRanging(true);
@@ -285,35 +315,72 @@ public class MonteCarloRunner {
         return equation_tile;
     }
 
+    /**
+     * Creates a mapping where each equation is associated with a listing of all of the simulations run against it. The list
+     * for each equation contains maps that associate the number of guesses used to the result of the equation.
+     *
+     * This gives us a data structure with the following structure:
+     * {
+     *     EquationObject1 : [
+     *      {
+     *         '1000': MonteCarloResult1000
+     *      },
+     *      {
+     *         '10000': MonteCarloResult10000
+     *      },
+     *      ...
+     *     ],
+     *     EquationObject2 : [
+     *      {
+     *          '1000': MonteCarloResult2_1000
+     *      },
+     *      ...
+     *     ],
+     *     ...
+     * }
+     * @param table_data results from running the monte carlo algorithm on the equations
+     * @return a LinkedHashMap of the format described above. We use a LinkedHashMap so that the order at which we
+     * insert the values will be maintained, that way when we rerun the simulation the equations do not change order.
+     */
     private static LinkedHashMap<Equation, ArrayList<HashMap<Integer, MonteCarloResult>>> create_equation_groups(ObservableList<MonteCarloResult> table_data){
         ArrayList<Equation> equations = get_equations(table_data);
 
         LinkedHashMap<Equation, ArrayList<HashMap<Integer, MonteCarloResult>>> equation_dict = new LinkedHashMap<>();
+        // Loop through all of the equations
         for(int index = 0; index< equations.size(); index++){
             Equation equation = equations.get(index);
 
             ArrayList<HashMap<Integer, MonteCarloResult>> equation_dict_list = new ArrayList<>();
+            // For each equation go through the data and grab the results from the equation
             for (int jPos = 0; jPos < table_data.size(); jPos++){
 
                 MonteCarloResult result = table_data.get(jPos);
                 HashMap<Integer, MonteCarloResult> result_dict = new HashMap<>();
                 if (result.equation == equation) {
                     result_dict.put(result.num_times.getValue(), result);
-
+                    // If our list of equation results does not contain the current result then we add it
                     if (!is_in(equation_dict_list, result_dict))
                         equation_dict_list.add(result_dict);
                 }
             }
+            // We have created our mapping for the equation so we add it to the dictionary that will be returned.
             equation_dict.put(equation, equation_dict_list);
         }
 
         return equation_dict;
     }
 
+    /**
+     * Helper method for determining if an ArrayList of Hashmaps contains the specified HashMap (result_dict).
+     * @param equation_dict_list the array list of hash maps to check in
+     * @param result_dict the hash map that should be searched for. For our use this is a dictionary with only one key
+     *                    and value.
+     * @return true if found, false otherwise
+     */
     private static boolean is_in(ArrayList<HashMap<Integer, MonteCarloResult>> equation_dict_list, HashMap<Integer, MonteCarloResult> result_dict){
         for(HashMap<Integer, MonteCarloResult> map: equation_dict_list){
             Object[] result_key_array = result_dict.keySet().toArray();
-            if(map.containsKey(result_key_array[0])){
+            if(map.containsKey((Integer)result_key_array[0])){
                 return true;
             }
         }
@@ -332,4 +399,7 @@ public class MonteCarloRunner {
 
     private static final int[] number_of_elements = {1000, 10000, 100000, 1000000};
     private static int last_active_tab;
+
+    private static final int width = 1200;
+    private static final int height = 850;
 }
